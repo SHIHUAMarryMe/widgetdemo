9ed63f03a22f2578141af4dd570261b79883de82
shihua <tangtong@deepin.com>
2017-08-02 17:35:35 +0800

    ensure use widget not qgraphicsscene

--- widgetdemo/mainwindow.cpp
+++ widgetdemo/mainwindow.cpp
@@ -1,11 +1,2 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
-}
-
-MainWindow::~MainWindow()
-{
-
-}
--- widgetdemo/mainwindow.h
+++ widgetdemo/mainwindow.h
@@ -1,15 +1,35 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
 
-class MainWindow : public QMainWindow
+#include <new>
+#include <memory>
+
+
+#include <QWidget>
+
+class QListView;
+class QRectF;
+
+
+class MainWindow : public QWidget
 {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    MainWindow(QWidget* parent = nullptr);
+    MainWindow(const MainWindow&)=delete;
+    MainWindow(MainWindow&&)=delete;
+
+    MainWindow& operator=(const MainWindow&)=delete;
+    MainWindow& operator=(MainWindow&&)=delete;
+
+
+private:
+    QListView* m_LeftListView{nullptr};
+    QListView* m_TopListView{nullptr};
+
+    std::unique_ptr<QRectF> m_TopRect;
+    std::unique_ptr<QRectF> m_LeftRect;
 };
 
 #endif // MAINWINDOW_H
